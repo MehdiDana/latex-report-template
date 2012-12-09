40424ec3c26f1bd547117e8ddf0d3dc276ebd6f2
m3dana <m3dana@gmail.com>
2012-12-09 14:24:17 +0000

    version 1

---
+++ codes/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+* Author: Mehdi Dana
+* Tutor : Dr Gary Allen
+*/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
